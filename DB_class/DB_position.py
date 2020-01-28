aa3358782469e7085d426b060a8af49702c53b68
ely28876 <funtag@naver.com>
2020-01-28 17:32:53 +0900

    2020-01-28 JSS   1. position db class 완성

--- DB_class/DB_position.py
+++ DB_class/DB_position.py
@@ -1,13 +1,40 @@
+import pickle
+import os
+
+
 class Positions:
-    __attribute = {
-        "level": 0,
-        "id": "",
-        "name": ""
-    }
+    position_list = []
     __position = {
         "attributeId": "",
         "attributeName": "",
         "explain": "",
-        "positionName": [__attribute, __attribute, __attribute]
     }
+
+    def checkAddOrUpdate(self, position):
+        # DB와 id 존재 유무 체크
+        row = next((index for (index, item) in enumerate(Positions.position_list)
+                    if item["attributeId"] == position["attributeId"]), None)
+        if row is None:
+            self.addPositionDB(position)
+            return "Add"
+        else:
+            self.updatePositionDB(row, position)
+            return "Update"
+
+    def addPositionDB(self, position):
+        Positions.position_list.append(position)
+
+    def updatePositionDB(self, row, position):
+        Positions.position_list[row]["attributeName"] = position["attributeName"]
+        Positions.position_list[row]["explain"] = position["explain"]
+
+    def saveDB(self):
+        path = os.path.dirname(os.path.abspath(__file__)) + '\DB\position.pkl'
+        with open(path, 'wb') as fout:
+            pickle.dump(Positions.position_list, fout)
+
+    def loadDB(self):
+        path = os.path.dirname(os.path.abspath(__file__)) + '\DB\position.pkl'
+        with open(path, 'rb') as fin:
+            Positions.position_list = pickle.load(fin)
 
