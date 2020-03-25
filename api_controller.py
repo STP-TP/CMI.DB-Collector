291c33a58febdd63664eefa90e89f0d956195cdd
ely28876 <funtag@naver.com>
2020-03-25 22:36:54 +0900

    Del: pickle database
    delete legacy code(pickle database)
    
    resolve #7 and close #1

--- api_controller.py
+++ api_controller.py
@@ -1,9 +1,5 @@
 import api_comm as comm
 import json
-import DB_class.DB_other as otherDb
-import DB_class.DB_user as userDb
-import DB_class.DB_match as matchDb
-import DB_class.DB_match_detail as matchDetailDb
 from DB_class.user_param.param_db import *
 import DB_class.user_param.param_path as path_define
 from DB_class.DB_parser import *
@@ -12,12 +8,6 @@ from DB_class.user_param.param_private import *
 
 
 class CollectDbFlow:
-    db_char = otherDb.GameCharacters()
-    db_item = otherDb.GameItems()
-    db_user = userDb.User()
-    db_match_normal = matchDb.MatchList(normal)
-    db_match_rating = matchDb.MatchList(rating)
-    db_match_detail = matchDetailDb.MatchDetailList()
     __db_collect_mode = False
     __sql = MysqlController(server_ip, server_id, server_pw, server_database)
     parser = ApiParser()
@@ -106,20 +96,6 @@ class CollectDbFlow:
         for save_target in param_match_detail_db:
             self.__sql.insert_match_detail(save_target)
 
-    def save_play_info_to_pickle(self, param_user_db, param_match_db, param_match_detail_db):
-        self.db_user.update_new_db_list(param_user_db)
-        self.db_user.save_db()
-        self.db_match_rating.update_new_db_list(param_match_db)
-        self.db_match_rating.save_db()
-        self.db_match_detail.update_new_db_list(param_match_detail_db)
-        self.db_match_detail.save_db()
-
-    def load_play_info_from_pickle(self):
-        self.db_user.load_db()
-        self.db_match_normal.load_db()
-        self.db_match_rating.load_db()
-        self.db_match_detail.load_db()
-
     def collect_character_db(self, save_on_off=False):
         """return: character db list """
         body = self.response_code(self.__com.get_character_info())
@@ -133,13 +109,13 @@ class CollectDbFlow:
         return local_character_db
 
     def collect_items(self):
-        char = self.db_char.get_db()
+        """char = self.db_char.get_db()
         for char_id in char:
             res = self.__com.search_item("E ", "front", 100, [char_id[character_id[api]]])
             body = json.loads(res["body"])
             for item in body.get("rows"):
                 self.db_item.overlap_check(item)
-        self.db_item.save_db()
+        self.db_item.save_db()"""
 
     def collect_rating_ranker_id(self, rank_min, rank_max):
         user_list = []
@@ -173,7 +149,6 @@ class CollectDbFlow:
             self.collect_game_information(user_list, temp_day_start, temp_day_end, rating)
 
         if self.__db_collect_mode:
-            # self.save_play_info_to_pickle(local_user_db, local_match_db, local_match_detail_db)
             self.save_play_info_to_sql(local_user_db, local_match_db, local_match_detail_db)
             print("DB Save End")
 
@@ -186,7 +161,6 @@ class CollectDbFlow:
             self.collect_game_information(user_list, temp_day_start, temp_day_end, normal)
 
         if self.__db_collect_mode:
-            # self.save_play_info_to_pickle(local_user_db, local_match_db, local_match_detail_db)
             self.save_play_info_to_sql(local_user_db, local_match_db, local_match_detail_db)
             print("DB Save End")
 
@@ -203,6 +177,5 @@ class CollectDbFlow:
             self.collect_game_information(user_list, temp_day_start, temp_day_end, param_game_type)
 
         if self.__db_collect_mode:
-            # self.save_play_info_to_pickle(local_user_db, local_match_db, local_match_detail_db)
             self.save_play_info_to_sql(local_user_db, local_match_db, local_match_detail_db)
             print("DB Save End")
